b6a11b5c17692519c77ec9c227862a0d679905a0
ryosukie1133-rgb <ryosukie1133@gmail.com>
2026-01-31 07:50:41 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+import datetime
+
+# นี่คือส่วนที่บอทจะทำงาน
+current_time = datetime.datetime.now()
+print(f"บอททำงานสำเร็จเมื่อเวลา: {current_time}")
+
+# ถ้าคุณมี Token บอท Discord หรือ LINE ให้ใส่คำสั่งรันตรงนี้
